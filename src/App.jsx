f38ed1e1d8d702da8f69e6bac2981b449244d28c
pankaj5417 <87115576+pankaj5417@users.noreply.github.com>
2022-03-22 17:41:30 +0530

    updating the category item and directory component

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import { Directory } from './components/category-item/Directory';
+const  App=()=> {
+ 
+
+  return (
+    <div className='categories-container'>
+   <Directory/>
+    </div>
+   
+  );
+}
+
+export default App;
